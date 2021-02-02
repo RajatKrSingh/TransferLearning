e7f2ae5ef8ed03b65eccae9ab740735a62063ed6
Rajat Singh <rajat.singh@sap.com>
2021-02-02 15:25:30 +0530

    EWC for CIFAR-10 added

--- ewc_cnn.py
+++ ewc_cnn.py
@@ -55,7 +55,7 @@ def get_fisherinformation(model,x_batch,y_batch):
         fisher_information = d2E_dW2
         is_first = True
 
-    for layer_nodes in range(6):   #Hardcoded for time being
+    for layer_nodes in range(12):   #Hardcoded for time being
         fisher_information[layer_nodes] = tf.add(tf.abs(fisher_information[layer_nodes]),tf.abs(d2E_dW2[layer_nodes]))
 
 
@@ -83,6 +83,24 @@ class CustomCallBack(keras.callbacks.Callback):
 
 # Create Neural Networks from keras package
 # Output: model - Initialized ANN with specified architecture
+def create_cnn_model_c10():
+    model = SequentialCustom()
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(32, 32, 3)))
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(10, activation='softmax'))
+    # compile model
+    opt = SGD(lr=0.01, momentum=0.9)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'],run_eagerly=True)
+    return model
+
+# Create Neural Networks from keras package
+# Output: model - Initialized ANN with specified architecture
 def create_cnn_model():
     model = SequentialCustom()
     model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
@@ -98,7 +116,55 @@ def create_cnn_model():
 # Call Builder Method for Fisher Information Computation of Subset Models
 # Inputs : None
 # Outputs: Locally Save Fisher Information Numpy variables for each model
-def training_models_fisher_computation(X_train1, Y_train1, X_train2, Y_train2, X_test1, Y_test1, X_test2, Y_test2):
+def training_models_fisher_computation_c10(X_train1, Y_train1, X_train2, Y_train2):
+
+    global fisher_information
+    fisher_information = {}
+    global is_first
+    is_first = False
+    #################################################
+    ####### Model 1 Training Parameters and Training
+    #################################################
+    ann_model1 = create_cnn_model_c10()
+
+    # Perform Training to get Fisher Information
+    fisher_callback = CustomCallBack()
+    ann_model1.fit(X_train1, Y_train1, epochs=1, batch_size=32, verbose=1, callbacks=[fisher_callback])
+
+    # Save Model for future use
+    ann_model1.save('ann_ewc_model1_c10.h5')
+
+    # Save numpy variables in File
+    layer_counter = 0
+
+    for layer_nodes in fisher_information:
+        np.save('Model1FI_c10/'+str(layer_counter),layer_nodes.numpy())
+        layer_counter = layer_counter+1
+
+    ###############################################
+    ##### Model 2 Training Parameters and Training
+    ###############################################
+    fisher_information = {}
+    is_first = False
+    ann_model2 = create_cnn_model_c10()
+
+    # Perform Training to get Fisher Information
+    fisher_callback = CustomCallBack()
+    ann_model2.fit(X_train2, Y_train2, epochs=1, batch_size=32, verbose=1, callbacks=[fisher_callback])
+
+    # Save Model for future use
+    ann_model2.save('ann_ewc_model2_c10.h5')
+
+    # Save numpy variables in File
+    layer_counter = 0
+    for layer_nodes in fisher_information:
+        np.save('Model2FI_c10/'+str(layer_counter),layer_nodes.numpy())
+        layer_counter = layer_counter+1
+
+# Call Builder Method for Fisher Information Computation of Subset Models
+# Inputs : None
+# Outputs: Locally Save Fisher Information Numpy variables for each model
+def training_models_fisher_computation(X_train1, Y_train1, X_train2, Y_train2):
 
     global fisher_information
     global is_first
@@ -154,20 +220,20 @@ def get_fisher_information():
     # Model 1 Parameters
     layer_weights_1 = {}
     layer_biases_1 = {}
-    for loadfile_counter in range(6):
+    for loadfile_counter in range(12):
         if loadfile_counter % 2 == 0:
-            layer_weights_1[(int)(loadfile_counter/2)] = np.load('Model1FI/'+str(loadfile_counter)+'.npy')
+            layer_weights_1[(int)(loadfile_counter/2)] = np.load('Model1FI_c10/'+str(loadfile_counter)+'.npy')
         else:
-            layer_biases_1[(int)(loadfile_counter/2)] = np.load('Model1FI/'+str(loadfile_counter)+'.npy')
+            layer_biases_1[(int)(loadfile_counter/2)] = np.load('Model1FI_c10/'+str(loadfile_counter)+'.npy')
 
     # Model 2 Parameters
     layer_weights_2 = {}
     layer_biases_2 = {}
-    for loadfile_counter in range(6):
+    for loadfile_counter in range(12):
         if loadfile_counter % 2 == 0:
-            layer_weights_2[(int)(loadfile_counter/2)] = np.load('Model2FI/'+str(loadfile_counter)+'.npy')
+            layer_weights_2[(int)(loadfile_counter/2)] = np.load('Model2FI_c10/'+str(loadfile_counter)+'.npy')
         else:
-            layer_biases_2[(int)(loadfile_counter/2)] = np.load('Model2FI/'+str(loadfile_counter)+'.npy')
+            layer_biases_2[(int)(loadfile_counter/2)] = np.load('Model2FI_c10/'+str(loadfile_counter)+'.npy')
 
     return layer_weights_1, layer_biases_1, layer_weights_2, layer_biases_2
 
@@ -179,10 +245,10 @@ def get_saved_model_weights():
     layer_weights_1 = {}
     layer_biases_1 = {}
 
-    ann_model1 = load_model('ann_ewc_model1.h5', custom_objects={'SequentialCustom': SequentialCustom})
+    ann_model1 = load_model('ann_ewc_model1_c10.h5', custom_objects={'SequentialCustom': SequentialCustom})
     layer_counter = 0
     for layer_number in range(len(ann_model1.layers)):
-        if layer_number == 1 or layer_number == 2:
+        if layer_number == 2 or layer_number == 5 or layer_number == 6:
             continue
         layer_weights_1[layer_counter] = ann_model1.layers[layer_number].get_weights()[0]
         layer_biases_1[layer_counter] = ann_model1.layers[layer_number].get_weights()[1]
@@ -192,16 +258,54 @@ def get_saved_model_weights():
     layer_weights_2 = {}
     layer_biases_2 = {}
 
-    ann_model2 = load_model('ann_ewc_model2.h5', custom_objects={'SequentialCustom': SequentialCustom})
+    ann_model2 = load_model('ann_ewc_model2_c10.h5', custom_objects={'SequentialCustom': SequentialCustom})
     layer_counter = 0
     for layer_number in range(len(ann_model2.layers)):
-        if layer_number == 1 or layer_number == 2:
+        if layer_number == 2 or layer_number == 5 or layer_number == 6:
             continue
         layer_weights_2[layer_counter] = ann_model2.layers[layer_number].get_weights()[0]
         layer_biases_2[layer_counter] = ann_model2.layers[layer_number].get_weights()[1]
         layer_counter = layer_counter + 1
 
     return layer_weights_1, layer_biases_1, layer_weights_2, layer_biases_2
+
+# Perform EWC and get resultant ANN
+# Inputs : None
+# Outputs: Resultant ANN using EWC
+def perform_symmetric_ewc_c10():
+    # Get Fisher Information
+    fisher_layer_weights_1, fisher_layer_biases_1, fisher_layer_weights_2, fisher_layer_biases_2 = get_fisher_information()
+
+    # Get model weights and parameters
+    layer_weights_1, layer_biases_1, layer_weights_2, layer_biases_2 = get_saved_model_weights()
+
+    # Get resultant weights using EWC technique  W(final) = (W(a)*F(a) + W(b)*F(b))/(F(a) + F(b))
+    layer_resultant_weights = {}
+    layer_resultant_biases = {}
+
+    for layer_counter in range(len(layer_weights_1)):
+
+        layer_resultant_weights[layer_counter] = np.divide(np.add(np.multiply(layer_weights_1[layer_counter],fisher_layer_weights_1[layer_counter]),
+                                                                  np.multiply(layer_weights_2[layer_counter],fisher_layer_weights_2[layer_counter])), np.add(fisher_layer_weights_1[layer_counter],fisher_layer_weights_2[layer_counter]))
+
+        layer_resultant_biases[layer_counter] = np.divide(np.add(np.multiply(layer_biases_1[layer_counter],fisher_layer_biases_1[layer_counter]),
+                                                                 np.multiply(layer_biases_2[layer_counter],fisher_layer_biases_2[layer_counter])), np.add(fisher_layer_biases_1[layer_counter],
+                                                                                                                                                          fisher_layer_biases_2[layer_counter]))
+
+        layer_resultant_weights[layer_counter] = np.nan_to_num(layer_resultant_weights[layer_counter])
+        layer_resultant_biases[layer_counter] = np.nan_to_num(layer_resultant_biases[layer_counter])
+
+    # Assign weights to new model
+    ann_output_model = create_cnn_model_c10()
+    layer_counter = 0
+    for layer_number in range(len(ann_output_model.layers)):
+        if layer_number == 2 or layer_number == 5 or layer_number == 6:  # Skip weight assignment for Pooling and Flatten Layers
+            continue
+        ann_output_model.layers[layer_number].set_weights([layer_resultant_weights[layer_counter],
+                                                           layer_resultant_biases[layer_counter]])
+        layer_counter = layer_counter + 1
+
+    return ann_output_model
 
 # Perform EWC and get resultant ANN
 # Inputs : None
@@ -241,9 +345,7 @@ def perform_symmetric_ewc():
 
     return ann_output_model
 
-
-def main():
-
+def perform_mnist_operations():
     # Get MNIST dataset
     X_train, Y_train, X_test, Y_test = ws.load_dataset_mnist()
 
@@ -263,6 +365,34 @@ def main():
     # Get Accuracy of Consolidated Model on test data and Compare with WS technique
     ws.get_accuracy(ann_output_model,np.append(X_test1,X_test2,axis=0),np.append(Y_test1,Y_test2,axis=0))
     ws.get_accuracy(ann_output_model2,np.append(X_test1,X_test2,axis=0),np.append(Y_test1,Y_test2,axis=0))
+
+def perform_cifar10_operations():
+
+    # Get training data
+    X_train,Y_train,X_test,Y_test = ws.get_cifar10_training_testdata()
+
+    # Split Training Data and Test data for Different Data Models
+    X_train1, Y_train1, X_train2, Y_train2, X_test1, Y_test1, X_test2, Y_test2 = ws.split_train_test_data_c10(X_train, Y_train, X_test, Y_test)
+
+    # Perform Training and saving of Fisher Information
+    #training_models_fisher_computation_c10(X_train1, Y_train1, X_train2, Y_train2)
+
+    # Perform Elastic Weight Consolidation for Result ANN
+    ann_output_model = perform_symmetric_ewc_c10()
+
+    #ann_model1 = load_model('ann_ewc_model1_c10.h5', custom_objects={'SequentialCustom': SequentialCustom})
+    #ann_model2 = load_model('ann_ewc_model2_c10.h5', custom_objects={'SequentialCustom': SequentialCustom})
+    #ann_output_model2 = ws.perform_weight_summation(ann_model1,ann_model2)
+
+    # Get Accuracy of Consolidated Model on test data and Compare with WS technique
+    ws.get_accuracy(ann_output_model,np.append(X_test1,X_test2,axis=0),np.append(Y_test1,Y_test2,axis=0))
+    ws.get_accuracy(ann_output_model,np.append(X_train1,X_train2,axis=0),np.append(Y_train1,Y_train2,axis=0))
+
+
+def main():
+    #perform_mnist_operations()
+    perform_cifar10_operations()
+
 
 
 if __name__ == "__main__":
